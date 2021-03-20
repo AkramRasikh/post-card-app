e9de1605c5059f01e007ffa6dcc42355b4b310ad
akram rasikh <akram.rasikh@gmail.com>
2021-03-20 20:17:57 +0000

    props determin component render

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: ['plugin:react/recommended', 'eslint:recommended', 'airbnb'],
   ignorePatterns: ['**/serviceWorker.js'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'jsx-quotes': [2, 'prefer-single'],
+    'react/forbid-prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { postsAPI, usersAPI } from './api/post-card-data';
+import PostCard from './components/post-card';
 import './App.css';
 
 const App = () => {
-  const [usersFullData, setFullUserData] = useState([]);
+  const [usersPostData, setUsersPostData] = useState([]);
   useEffect(async () => {
     const { data: userData } = await usersAPI();
     const { data: postData } = await postsAPI();
 
-    const postsManipulated = postData.map(({ userId, ...props }) => {
-      const userPersonalData = userData.find((user) => userId === user.id);
+    const postsWithUserData = postData.map(({ userId, ...props }) => {
+      const userPersonalData = userData.find(({ id }) => userId === id);
       return {
         ...props,
         ...userPersonalData,
       };
     });
-    setFullUserData(postsManipulated);
+    setUsersPostData(postsWithUserData);
   }, []);
 
-  return usersFullData ? (
-    <div className='App'>Post card app</div>
+  return usersPostData.length > 0 ? (
+    <div className='App'>
+      {usersPostData.map(({ id, ...postData }) => (
+        <PostCard key={id} {...postData} />
+      ))}
+    </div>
   ) : (
     <div>...Loading</div>
   );
