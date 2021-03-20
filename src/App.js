cb1ddf19487185a7e865bb37369cffbb87abc82f
akram rasikh <akram.rasikh@gmail.com>
2021-03-20 20:01:25 +0000

    data correctly manipulated and merged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { postsAPI, usersAPI } from './api/post-card-data';
 import './App.css';
 
-const App = () => <div className='App'>Post card app</div>;
+const App = () => {
+  const [usersFullData, setFullUserData] = useState([]);
+  useEffect(async () => {
+    const { data: userData } = await usersAPI();
+    const { data: postData } = await postsAPI();
+
+    const postsManipulated = postData.map(({ userId, ...props }) => {
+      const userPersonalData = userData.find((user) => userId === user.id);
+      return {
+        ...props,
+        ...userPersonalData,
+      };
+    });
+    setFullUserData(postsManipulated);
+  }, []);
+
+  return usersFullData ? (
+    <div className='App'>Post card app</div>
+  ) : (
+    <div>...Loading</div>
+  );
+};
 
 export default App;
---
+++ src/api/post-card-data.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const usersAPI = async () => {
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+    return response;
+  } catch (error) {
+    console.log('error: ', error);
+    return false;
+  }
+};
+
+export const postsAPI = async () => {
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts',
+    );
+    return response;
+  } catch (error) {
+    console.log('error: ', error);
+    return false;
+  }
+};
